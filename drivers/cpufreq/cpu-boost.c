a472dc3ce91abd0778c1d952eca9f84bb487e8f5
Adhitya Mohan <me@adhityamohan.in>
2020-04-11 13:36:03 +0530

    Add extra guard code

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -211,11 +211,12 @@ static void do_input_boost(struct work_struct *work)
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 	cancel_delayed_work_sync(&input_boost_rem);
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (stune_boost_active) {
 		reset_stune_boost("top-app", boost_slot);
 		stune_boost_active = false;
 	}
-
+#endif
 	/* Set the input_boost_min for all CPUs in the system */
 	pr_debug("Setting input boost min for all CPUs\n");
 	for_each_possible_cpu(i) {
