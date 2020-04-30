74013514bebddf8689acf20a785fe6b9eba27a81
Adhitya Mohan <me@adhityamohan.in>
2020-04-30 20:12:41 +0530

    Fix up cpusetassists target array

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1779,14 +1779,13 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 {
 #ifdef CONFIG_CPUSETS_ASSIST
 	static struct cs_target cs_targets[] = {
-		/* Little-only cpusets go first */
+		{ "audio-app",		"0-3,6-7" },
 		{ "background",		"0-1" },
-		{ "audio-app",		"0-3"},
-		{ "camera-daemon",	"0-3" },
-		{ "system-background",	"0-3" },
-		{ "restricted",		"0-5" },
-		{ "top-app",		"0-7" },
+		{ "camera-daemon",	"0-3,6-7" },
 		{ "foreground",		"0-3,6-7" },
+		{ "restricted",		"0-3" },
+		{ "system-background",	"0-3" },
+		{ "top-app",		"0-7" },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
